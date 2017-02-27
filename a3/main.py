d4d4fb3a04e0c75ee70fd8db5eaaf8f987f1e375
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-02-27 18:55:09 +0000

    created a function for reading the url. Not reading all the lines, will change after parsing is introduced.

--- a3/main.py
+++ a3/main.py
@@ -3,90 +3,56 @@ Created on 27 Feb 2017
 
 @author: pigna
 '''
-'''
-
-Ideas for the code
-Read list, get size of the grid.
-
-Initialise to 0 every cell.
-
-
-3 functions:turn on, turn off, switch
-
-
-def on(grid)
-     for i in range (set by interval wanted)
-      for j n range (set by interval wanted)
-        if grid[i,j]!= on
-            change element to on
-        else
-            leave on
-
-def off(grid) Same thing, but off
-
-
-
-def switch(grid)
-    for i in range (set by interval wanted)
-      for j n range (set by interval wanted)
-        if grid[i,j]== on
-            change element to off
-        elif grid[i,j]==off
-            change element to off
-        else
-            print(there is an error in this coordinate)
-'''
 import urllib.request
-uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
-req = urllib.request.urlopen(uri)
-buffer = req.read().decode('utf-8')
 
-
-
-
+def read_url(url):
+    uri= url
+    req = urllib.request.urlopen(uri)
+    buffer = req.read().decode('utf-8').split("\n")
+    array_size = buffer[0]
+    return array_size
 
 class led_grid:
     def  __init__(self,size):
         self.size= size
         self.grid=[[False]*size for _ in range(0,size)]
-        print(self.grid)
                 
-    def turn_on(self):
+    def turn_on(self,x1,y1,x2,y2):
         
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.grid[i][j]==False:
-                    self.grid[i][j]=True
+        for i in range(y1,y2):
+            for j in range(x1,x2):
+                self.grid[i][j]=True
                
-        print(self.grid)  
+            print(self.grid[i])  
                
     
-    def turn_off(self):
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.grid[i][j]==True:
-                    self.grid[i][j]=False
+    def turn_off(self,x1,y1,x2,y2):
+        for i in range(y1,y2):
+            for j in range(x1,x2):
+                self.grid[i][j]=False
                
-        print(self.grid)  
+                print(self.grid[0])    
     
-    def switch(self):
-        for i in range(self.size):
-            for j in range(self.size):
+    def switch(self,x1,y1,x2,y2):
+        for i in range(y1,y2):
+            for j in range(x1,x2):
                 if self.grid[i][j]==True:
                     self.grid[i][j]=False
                 elif self.grid[i][j]==False:
                     self.grid[i][j]=True
                
-        print(self.grid)
+
+
+
+def main():
+    read_url("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+    print(read_url("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"))
+    c=led_grid(int(read_url("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")))
+    c.turn_on(1,0,2,2)
+    c.switch(0,0,5,2)
         
         
 
 if __name__ == '__main__':
-    
-    c=led_grid(3)
-    c.turn_on()
-    c.turn_off()
-    c.switch()
-    c.switch()
-    c.turn_off()
+    main()
     
