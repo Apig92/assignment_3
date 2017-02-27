44a28a0cf45b1dbcdb33e0fcf7e2a440472d75ae
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-02-27 11:26:08 +0000

    testing commit

--- a3/main.py
+++ a3/main.py
@@ -36,3 +36,25 @@ def switch(grid)
         else
             print(there is an error in this coordinate)
 '''
+import urllib.request
+uri = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+req = urllib.request.urlopen(uri)
+buffer = req.read().decode('utf-8')
+
+
+
+n=10
+a=[[0]*n for _ in range(n)]
+print(a[2:5])
+
+
+class led_grid:
+    def turn_on(self):
+        pass
+    def turn_off(self):
+        pass
+    def switch(self):
+        pass
+
+    
+    
