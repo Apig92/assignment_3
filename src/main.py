50f5667afc90696355dd4760c129db581dbd3958
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-02-28 18:11:37 +0000

    REGEX implemented, now the code can read a file, and get the content.

--- src/main.py
+++ src/main.py
@@ -6,15 +6,29 @@ Created on 27 Feb 2017
 
 import urllib.request
 import argparse
+import re
 
 
 def read_url(url):
     uri= url
     req = urllib.request.urlopen(uri)
-    buffer = req.read().decode('utf-8').split("\n")
-    array_size = buffer[0]
-    commands= buffer[1:]
-    return array_size,commands
+    content = req.read().decode('utf-8').split("\n")
+    array_size = content[0]
+    instructions=[]
+    for i in range(1,len(content)-1):
+        pattern=re.compile("(turn on|turn off|switch)\s*,*\s*(-{0,1}\d+)\s*,*\s*(-{0,1}\d+)\s*through\s*(-{0,1}\d+)\s*,*\s*(-{0,1}\d+)")
+        parsed_content=pattern.match(content[i]).groups()
+        if parsed_content:
+            instructions.append(parsed_content)
+    return array_size,instructions
+
+def read_instructions(command):
+    pattern=re.compile("(turn on|turn off|switch)\s*(-{0,1}\d+),(-{0,1}\d+) through (-{0,1}\d+),(-{0,1}\d+)")
+    parsed_content=pattern.match(command).group()
+    instructions=[]
+    if parsed_content:
+        instructions.append(parsed_content)
+    return instructions
 
 class led_grid:
     def  __init__(self,size):
@@ -27,10 +41,7 @@ class led_grid:
             for j in range(x1,x2):
                 self.grid[i][j]=True
                
-        return self.grid
-            
-              
-               
+        return self.grid           
     
     def turn_off(self,x1,y1,x2,y2):
         for i in range(y1,y2):
@@ -55,13 +66,34 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', help='input help')
     args = parser.parse_args()
-    filename = args.input
-    gridsize=read_url(filename)[0]
-    print(gridsize)
+    urlname = args.input
+    gridsize=read_url(urlname)[0]
+    content=read_url(urlname)[1]
     c=led_grid(int(gridsize))
-    c.turn_on(1,0,4999,2)
-    print(c.grid[0])
-    print(c.grid[1])
+    oncounter=0
+    offcounter=0
+    switchcounter=0
+    for i in range(len(content)):
+        command,x1,y1,x2,y2=content[i][0],int(content[i][1]),int(content[i][2]),int(content[i][3]),int(content[i][4])
+        if command=='turn on':
+            oncounter+=1
+            c.turn_on(x1,y1,x2,y2)
+
+        elif command=='turn off':
+            offcounter+=1
+            c.turn_off(x1,y1,x2,y2)
+           
+        elif command=='switch':
+            switchcounter+=1
+            c.switch(x1,y1,x2,y2)
+           
+    print("on:",oncounter,"off:",offcounter,"Switch:",switchcounter)
+       
+    #for i in range(0,len(content)):
+    #   print(content[i])
+    
+    print(c.grid[5])
+    #print(c.grid[6])
     #c.switch(0,0,5,3)
         
         
