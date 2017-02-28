634d55673b306b8d636e70bffc47ffb44577a95f
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-02-28 10:36:49 +0000

    now setup works correctly with the input, from the command line and the amazon instance.

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ Created on 30 Jan 2017
 '''
 from setuptools import setup
 
-setup(name="led_tester",
+setup(name="solveled",
       version="0.1",
       description="Assignment 3, COMP30670. It tests LEDs on a square grid.",
       url="",
       author="Andrea Pignanelli",
       author_email="andrea.pignanelli01@ucdconnect.ie",
       licence="GPL3",
-      packages=['a3'],
+      packages=['src'],
       entry_points={
-          'console_scripts':['solve_led=a3.main:main']
+          'console_scripts':['solveled=src.main:main']
           },
       install_requires=[
           'numpy',
