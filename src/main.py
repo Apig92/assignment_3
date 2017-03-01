d4e62e49eeb1f1c20cd50caf9804d70a8c9f1e5c
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-03-01 12:29:59 +0000

    All the files are read and processed correctly

--- src/main.py
+++ src/main.py
@@ -16,9 +16,9 @@ def read_url(url):
     array_size = content[0]
     instructions=[]
     for i in range(1,len(content)-1):
-        pattern=re.compile("(turn on|turn off|switch)\s*,*\s*(-{0,1}\d+)\s*,*\s*(-{0,1}\d+)\s*through\s*(-{0,1}\d+)\s*,*\s*(-{0,1}\d+)")
-        parsed_content=pattern.match(content[i]).groups()
-        if parsed_content:
+        pattern=re.compile("\s*(turn on|turn off|switch)\s*,*\s*(-{0,1}\d+)\s*,*\s*(-{0,1}\d+)\s*through\s*(-{0,1}\d+)\s*,*\s*(-{0,1}\d+)")
+        if pattern.match(content[i]) != None:
+            parsed_content=pattern.match(content[i]).groups()
             instructions.append(parsed_content)
     return array_size,instructions
 
@@ -40,6 +40,7 @@ class led_grid:
         for i in range(y1,y2+1):
             for j in range(x1,x2+1):
                 self.grid[i][j]=False
+                
         return self.grid  
     
     def switch(self,x1,y1,x2,y2):
@@ -66,21 +67,64 @@ def main():
     oncounter=0
     offcounter=0
     switchcounter=0
+    minus=0
+    over=0
     for i in range(len(content)):
         command,x1,y1,x2,y2=content[i][0],int(content[i][1]),int(content[i][2]),int(content[i][3]),int(content[i][4])
+        
+            
+        if x1<0:
+            x1=0
+            #print(content[i])
+            #print(x1)
+            minus+=1
+        if x2<0:
+            x2=0
+            #print(content[i])
+            #print(x1)
+            minus+=1   
+        
+        if x2>(int(gridsize)-1):
+            x2=int(gridsize)-1
+            over+=1
+            print(content[i])
+    
+        
+        if x1>(int(gridsize)-1):
+            x1=int(gridsize)-1
+            over+=1
+            print(content[i])
+            
+        if y1<0:
+            y1=0
+            #print(content[i])
+            #print(y1)
+            minus+=1
+        
+        if y2>(int(gridsize)-1):
+            y2=int(gridsize)-1
+            over+=1
+            print(content[i])
+        
+        if y2<0:
+            y2=0
+        
+        if y1>(int(gridsize)-1):
+            y1=int(gridsize)-1
+            over+=1
+            print(content[i])
+            
         if command=='turn on':
             oncounter+=1
             c.turn_on(x1,y1,x2,y2)
-
         elif command=='turn off':
             offcounter+=1
             c.turn_off(x1,y1,x2,y2)
-           
         elif command=='switch':
             switchcounter+=1
             c.switch(x1,y1,x2,y2)
            
-    print("on:",oncounter,"off:",offcounter,"Switch:",switchcounter)
+    print("on:",oncounter,"off:",offcounter,"Switch:",switchcounter,"minus",minus,"over",over)
        
     #for i in range(0,len(content)):
     #   print(content[i])
