60580243ff7817c5c04518f5e7080a822ffd4983
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-03-01 20:01:10 +0000

    Moved the clean_up function inside to have it as a grid class method. I also added the test.

--- src/main.py
+++ src/main.py
@@ -24,26 +24,7 @@ def read_url():
         if pattern.match(content[i]) != None:
             parsed_content=pattern.match(content[i]).groups()
             instructions.append(parsed_content)
-    return array_size,instructions,urlname
-
-def clean_up(gridsize,x1,y1,x2,y2):
-    if x1<0:
-        x1=0
-    if x2<0:
-        x2=0   
-    if x2>(int(gridsize)-1):
-        x2=int(gridsize)-1
-    if x1>(int(gridsize)-1):
-        x1=int(gridsize)-1    
-    if y1<0:
-        y1=0
-    if y2>(int(gridsize)-1):
-        y2=int(gridsize)-1
-    if y2<0:
-        y2=0
-    if y1>(int(gridsize)-1):
-        y1=int(gridsize)-1
-    return x1,y1,x2,y2
+    return urlname,int(array_size),instructions
 
 class led_grid:
     def  __init__(self,size):
@@ -72,6 +53,25 @@ class led_grid:
                     self.grid[i][j]=True
         return self.grid
     
+    def clean_up(self,x1,y1,x2,y2):
+        if x1<0:
+            x1=0
+        if x2<0:
+            x2=0   
+        if x2>(self.size-1):
+            x2=self.size-1
+        if x1>(self.size-1):
+            x1=self.size-1    
+        if y1<0:
+            y1=0
+        if y2>(self.size-1):
+            y2=self.size-1
+        if y2<0:
+            y2=0
+        if y1>(self.size-1):
+            y1=self.size-1
+        return x1,y1,x2,y2
+    
     def execute_instructions(self,command,x1,y1,x2,y2):
         if command=='turn on':
                 self.turn_on(x1,y1,x2,y2)
@@ -88,16 +88,15 @@ class led_grid:
                     if self.grid[i][j]==True:
                         LEDS+=1
         return LEDS
-                    
+               
 def main():
-    urlname=read_url()[2]
+    urlname=read_url()[0]
     if urlname.startswith('http://'):
-        gridsize,content=read_url()[0],read_url()[1]
-        c=led_grid(int(gridsize))
+        gridsize,content=read_url()[1],read_url()[2]
+        c=led_grid(gridsize)
         for i in range(len(content)):
             command,x1,y1,x2,y2=content[i][0],int(content[i][1]),int(content[i][2]),int(content[i][3]),int(content[i][4])  
-            cc=clean_up(gridsize, x1, y1, x2, y2)
-            c.execute_instructions(command,cc[0],cc[1],cc[2],cc[3])  
+            c.execute_instructions(command,c.clean_up(x1, y1, x2, y2)[0],c.clean_up(x1, y1, x2, y2)[1],c.clean_up(x1, y1, x2, y2)[2],c.clean_up(x1, y1, x2, y2)[3])  
         print(urlname,c.count_leds(),"\nRuntime %s seconds" % (time.time() - start_time))
     else:
         print("Please enter a valid url(http:// included)")
--- tests/tests.py
+++ tests/tests.py
@@ -24,6 +24,17 @@ class Testgrid(unittest.TestCase):
         t.turn_on(0, 0, 2, 2)
         self.assertTrue(t.switch(0, 0, 2, 2)==[[False,False,False],[False,False,False],[False,False,False]])
         self.assertTrue(t.switch(0, 0, 1, 1)==[[True,True,False],[True,True,False],[False,False,False]])
+    
+    def test_clean_up(self):
+        t= led_grid(3)
+        self.assertTrue(t.clean_up(-1, 1, 2, 13)[0]==0)
+        self.assertTrue(t.clean_up(-1, 1, 2, 133)[3]==2)
+        self.assertFalse(t.clean_up(-1, 1, 2, 2)[0]==-1)
+
+    def test_execute(self):
+        t= led_grid(3)
+        self.assertTrue(t.execute_instructions("switch",0, 0, 1, 1)==[[True,True,False],[True,True,False],[False,False,False]])
+        self.assertFalse(t.execute_instructions("turn on",0, 0, 2, 2)==[[False,False,False],[False,False,False],[False,False,False]])
         
     def test_count(self):
         t= led_grid(3)
@@ -31,11 +42,9 @@ class Testgrid(unittest.TestCase):
         self.assertTrue(t.count_leds()==9)
         t.turn_off(0, 0, 2, 2) 
         self.assertTrue(t.count_leds()==0)
+    
         
-    def test_execute(self):
-        t= led_grid(3)
-        self.assertTrue(t.execute_instructions("switch",0, 0, 1, 1)==[[True,True,False],[True,True,False],[False,False,False]])
-        self.assertFalse(t.execute_instructions("turn on",0, 0, 2, 2)==[[False,False,False],[False,False,False],[False,False,False]])
+    
 
         
 
